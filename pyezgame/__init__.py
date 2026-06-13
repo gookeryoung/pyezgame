a2ed6a6673d4da1abe0f2dfdec15876e31ed0615
gooker_young <gooker_young@qq.com>
2026-06-13 12:13:36 +0800

    chore(release): 更新版本号从 0.1.2 到 0.1.3
    - 更新 pyproject.toml 中的版本号
    - 更新 __init__.py 中的版本号

--- pyezgame/__init__.py
+++ pyezgame/__init__.py
@@ -9,5 +9,5 @@ from ._pyezgame import *  # noqa: F403
 from ._pyezgame import GameLib
 from .utils import get_respath
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __all__ = ["GameLib", "get_respath"]
