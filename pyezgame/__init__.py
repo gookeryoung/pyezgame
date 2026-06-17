fe7c3419577e6bb6c4a49e5ae8a2caeeedca96f2
gooker_young <gooker_young@qq.com>
2026-06-17 12:54:40 +0800

    chore: bump version to 0.4.3

--- pyezgame/__init__.py
+++ pyezgame/__init__.py
@@ -9,5 +9,5 @@ from ._pyezgame import *  # noqa: F403
 from ._pyezgame import GameLib
 from .utils import get_respath
 
-__version__ = "0.4.1"
+__version__ = "0.4.3"
 __all__ = ["GameLib", "get_respath"]
