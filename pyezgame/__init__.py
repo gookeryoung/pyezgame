9795a429acc8dfc665a4d45cab4d37eef18dd357
gooker_young <gooker_young@qq.com>
2026-06-17 08:39:40 +0800

    feat(core): 更新版本号到 0.4.0
    提示: 移除对python3.8的支持!

--- pyezgame/__init__.py
+++ pyezgame/__init__.py
@@ -9,5 +9,5 @@ from ._pyezgame import *  # noqa: F403
 from ._pyezgame import GameLib
 from .utils import get_respath
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __all__ = ["GameLib", "get_respath"]
